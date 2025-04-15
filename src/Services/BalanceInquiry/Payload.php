d1c5c3617c28326341a7e7ded24088eb1536fccd
Ata <quetzalskeith@gmail.com>
2025-04-15 16:26:33 +0700

    Added balance inquiry

---
+++ src/Services/BalanceInquiry/Payload.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace QuetzalStudio\PhpSnapBi\Services\InternalAccountInquiry;
+
+use QuetzalStudio\PhpSnapBi\Contracts\AdditionalInfo;
+use QuetzalStudio\PhpSnapBi\Contracts\ServicePayload;
+
+class Payload implements ServicePayload
+{
+    public function __construct(
+        public ?string $partnerReferenceNo = null,
+        public ?string $bankCardToken = null,
+        public ?string $accountNo = null,
+        public ?array $balanceTypes = null,
+        public AdditionalInfo|array|null $additionalInfo = null,
+    ) {
+        //
+    }
+
+    public function toArray(): array
+    {
+        $data = [
+            'partnerReferenceNo' => $this->partnerReferenceNo,
+            'bankCardToken' => $this->bankCardToken,
+            'accountNo' => $this->accountNo,
+            'balanceTypes' => $this->balanceTypes,
+        ];
+
+        if ($this->additionalInfo) {
+            $data['additionalInfo'] = $this->additionalInfo instanceof AdditionalInfo
+                ? $this->additionalInfo->toArray()
+                : $this->additionalInfo;
+        }
+
+        foreach ($data as $key => $val) {
+            if (is_null($val)) {
+                unset($data[$key]);
+            }
+        }
+
+        return $data;
+    }
+}
